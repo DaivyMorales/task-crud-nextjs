4740655a107546df11416569bd8d3fd8d210e954
Daivy Morales <daivymorales7705@gmail.com>
2023-03-19 19:35:55 -0500

    Add Cor

--- next.config.js
+++ next.config.js
@@ -1,6 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async headers() {
+    return [
+      {
+        // habilitar CORS para localhost:3000 y https://task-crud-nextjs.vercel.app
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://task-crud-nextjs.vercel.app, http://localhost:3000",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-Requested-With,Access-Control-Allow-Origin, X-HTTP-Method-Override, Content-Type, Authorization, Accept",
+          },
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
